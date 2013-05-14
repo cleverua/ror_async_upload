ff39800356875cb4d222a1b6a3ea107401fb5755
Artem Frolov <artem@cleverua.com>
2013-05-14 15:58:42 +0300

    typo fix

--- Ror/lib/stalker_job/job.rb
+++ Ror/lib/stalker_job/job.rb
@@ -11,7 +11,7 @@ job "uploader.create_fs_s3_photo_from_fs_photo" do |ids|
   unless photo.nil?
     save_time = Benchmark.realtime { photo.update_attribute( :s3_photo, photo.fs_photo) }
     photo.statistics.update_attribute( :fs_s3_photo_stats, save_time )
-    FileUtils.remove_dir("#{Rails.root}/public/fs_uploads/photo/fs_photo/#{date.id}", :force => true)
+    FileUtils.remove_dir("#{Rails.root}/public/fs_uploads/photo/fs_photo/#{photo.id}", :force => true)
     photo.remove_fs_photo!
     photo.save
   end
